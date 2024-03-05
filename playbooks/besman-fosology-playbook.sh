cb4bfb61529f3204af6e09540e3ec19940485a4c
samirparhi-dev <samirparhi@gmail.com>
2024-03-05 14:02:32 +0530

    created fossology playbook

--- playbooks/besman-fosology-playbook.sh
+++ playbooks/besman-fosology-playbook.sh
@@ -2,7 +2,7 @@
 # Author: Samir Ranjan Parhi
 # License: Same as Repository Licence
 # Usage : Beta-Release
-# Date : 12/02/2024
+# Date : 1/03/2024
 
 set -e
 RED=$(tput setaf 1)
@@ -21,7 +21,7 @@ echo -e "\n${BLUE}###################################################${RESET}"
 
 # Lifecycle functions
 function __besman_init {
-
+    
    # Check Prerequsite for this CLI to run
    echo -e "\n${BLUE}🛫 Lets do a Quick check if you are running Any container runtime .${RESET}"
 
@@ -31,83 +31,111 @@ function __besman_init {
         exit 1
     fi
 
-    # Check if SonarQube container is running
+    # Check if Fossology container is running
 
-    if docker ps -a --format '{{.Names}}' | grep -Eq '^sonarqube$'; then
-        echo -e "\n${BLUE}✅ SonarQube container is already running.${RESET}"
+    if docker ps -a --format '{{.Names}}' | grep -Eq '^fossology$'; then
+        echo -e "\n${BLUE}✅ Fossology container is already running.${RESET}"
     else
-        echo -e "\n${BLUE}🛫 Starting SonarQube container...${RESET}"
-        docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
-    fi
+        echo -e "\n${BLUE}🛫 Starting fossology container...${RESET}"
 
-    # Check if Sonar Scanner CLI is installed
-    if ! command -v sonar-scanner &> /dev/null; then
-        echo -e "\n${BLUE}❌ Sonar Scanner CLI is not installed. Please install Sonar Scanner CLI before running this script.${RESET}"
-        exit 1
+        docker run --name fossology -p 8081:80 --d fossology/fossology
     fi
 
 }
 
-
-
 function __besman_execute {
 
-    PWD=$(pwd)
-    SONAR_REPORT_PATH=  
-    # Run SonarQube scan
-
-    echo -e "\n${BLUE}🛫 Starting SonarQube scan...${RESET}"
-
     # Define variables
+    PWD=$(pwd)
+    PROJECT_NAME="$1"
+    FOSSOLOGY_HOST_URL_WITH_PORT_NO="$2"
+    FOSSOLOGY_TOKEN="$3"
 
-    PROJECT_KEY="$1"
-    PROJECT_NAME="$2"
-    SONAR_HOST_URL="$3"
-    REPORT_FOLDER="$4"
+    mkdir $PWD/$PROJECT_NAME
 
-    #Actual scanning command for sonar scanner
+    echo -e "\n${BLUE}🛫 Starting Fossology Analysis...${RESET}"
 
-    # sonar-scanner -X \
-    sonar-scanner \
-    -Dsonar.projectKey=$PROJECT_KEY \
-    -Dsonar.projectName="$PROJECT_NAME" \
-    -Dsonar.host.url=$SONAR_HOST_URL \
-    -Dsonar.sources=. \
-    -Dsonar.python.binaries=python \
-    # -Dsonar.analysis.report.format=json \
-    -Dsonar.login=$SONAR_LOGIN_TOKEN
+   
+
+    curl -k -s -S -X POST http://FOSSOLOGY_HOST_URL_WITH_PORT_NO/repo/api/v1/folders \
+    -H "parentFolder: 1" \
+    -H "folderName: $PROJECT_NAME" \
+    -H "Authorization: Bearer $FOSSOLOGY_TOKEN"
+
+    curl -k -s -S -X GET http://localhost:8081/repo/api/v1/folders \
+    -H "Authorization: Bearer $FOSSOLOGY_TOKEN"
+
+    curl -k -s -S -X POST http://localhost:8081/repo/api/v1/uploads \
+    -H "folderId: 4" \
+    -H "uploadDescription: Created for $PROJECT_NAME" \
+    -H "public: public' -H 'Content-Type: multipart/form-data" \
+    -F "fileInput=@"$PWD-$PROJECT_NAME.tar.gz";type=application/octet-stream" \
+    -H "Authorization: Bearer $FOSSOLOGY_TOKEN"
+
+    curl -k -s -S -X POST http://localhost:8081/repo/api/v1/jobs \
+    -H "folderId: 4" \
+    -H "uploadId: 3" \
+    -H "Authorization: Bearer $FOSSOLOGY_TOKEN" \
+    -H 'Content-Type: application/json'
+        --data '{
+        "analysis": {
+            "bucket": true,
+            "copyright_email_author": true,
+            "ecc": true, "keyword": true,
+            "mime": true,
+            "monk": true,
+            "nomos": true,
+            "package": true
+        },
+        "decider": {
+            "nomos_monk": true,
+            "bulk_reused": true,
+            "new_scanner": true
+        }
+        }'
+
+    curl -k -s -S -X GET http://localhost:8081/repo/api/v1/report \
+    -H "uploadId: 3" -H 'reportFormat: spdx2' \
+    -H "Authorization: Bearer $FOSSOLOGY_TOKEN" \
+
+    curl -k -s -S -X GET http://localhost:8081/repo/api/v1/report/6 \
+    -H "Authorization: Bearer $FOSSOLOGY_TOKEN" \
+    -H 'accept: text/plain' > $PWD/$PROJECT_NAME/$PROJECT_NAME_report.rdf.xml
 
     if [ $? -eq 0 ]; then
-        echo -e "\n${BLUE}✅ Sonar-Scanner for ${PROJECT_NAME} project  successfully! \n the report stored at ${PWD}/.scannerwork${RESET}"
+        echo -e "\n${BLUE}✅ Fossology for ${PROJECT_NAME} project  successfully! \n the report stored at ${PWD}/${PROJECT_NAME}${RESET}"
     else
-        echo -e "\n${BLUE}❌ Sonar-Scanner failed ${PROJECT_NAME} project  successfully!${RESET}"
+        echo -e "\n${BLUE}❌ Fossology Analysis failed for ${PROJECT_NAME} project!${RESET}"
     fi
 }
 function __besman_prepare {
 
-   echo -e "\n${BLUE}✅ preaparing Sonar-Scanner report!${RESET}"
+   echo -e "\n${BLUE}✅ Preparing Fossology report!${RESET}"
  }
 
 function __besman_publish {
 
-   echo -e "\n${BLUE}✅ Placeholder for Sonar Report to BES Data Store!${RESET}"
+   echo -e "\n${BLUE}✅ Placeholder for Fossology Report to BES Data Store!${RESET}"
  }
 
 function __besman_cleanup {
 
     echo -e "\n${BLUE}✅ Cleaning-UP and Reclaiming your Used space !${RESET}"
-    # Stop SonarQube container
-    echo -e "\n${BLUE}Stopping SonarQube container...${RESET}"
+    # Stop Fossology container
+    echo -e "\n${BLUE}Stopping Fossology container...${RESET}"
 
-    docker stop sonarqube
+    docker stop fossology
 
     sleep 10
+    
 
     if [ $? -eq 0 ]; then
-        echo -e "\n${BLUE}✅ SonarQube Container Stopped successfully.${RESET}"
+        echo -e "\n${BLUE}✅ Fossology Container Stopped successfully.${RESET}"
     else
-        echo -e "\n${BLUE}❌ Sorry was not able to stop info for Sonar-Qube container.😊 No Worries you can check it using ${BOLD}${GREEN}docker ps --format '{{.Names}}${RESET}'${RESET}"
+        echo -e "\n${BLUE}❌ Sorry was not able to stop info for Fossology container.😊 No Worries you can check it using ${BOLD}${GREEN}docker ps --format '{{.Names}}${RESET}'${RESET}"
     fi
+
+    rm -rf $PWD/$PROJECT_NAME
  }
 
 function __besman_launch {
