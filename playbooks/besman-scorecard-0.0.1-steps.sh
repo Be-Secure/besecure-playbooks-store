e7093fa5d75a646101c6cb4117087b26abbd371c
sand-hya <sandhya.k91@wipro.com>
2024-04-08 13:45:25 +0530

    scorecard playbook

---
+++ playbooks/besman-scorecard-0.0.1-steps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+    echo "Running $ASSESSMENT_TOOL_NAME"
+    cd "$BESMAN_TOOL_PATH" || return 1
+    
+    curl -X 'GET' \
+    "https://api.securityscorecards.dev/projects/github.com/Be-Secure/$github_repo_name" \
+    -H "accept: application/json" >> $BESMAN_ARTIFACT_NAME-$BESMAN_ARTIFACT_VERSION-scorecard.json
+    #formatting scorecard report
+    python3 -m json.tool $BESMAN_ARTIFACT_NAME-$BESMAN_ARTIFACT_VERSION-scorecard.json > $BESMAN_ARTIFACT_NAME-$BESMAN_ARTIFACT_VERSION-scorecard.json
+
+    if [[ "$?" != "0" ]] 
+    then
+        export SCORECARD_RESULT=1
+    else
+        export SCORECARD_RESULT=0
+    fi 
