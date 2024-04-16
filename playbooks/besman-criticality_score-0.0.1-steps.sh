ecc5b4a78756c606d7d4874640156f3768cf7521
Neeraj Kumar <neeraj.kumar32@wipro.com>
2024-04-16 09:56:02 +0530

    criticality_score playbook (#27)
    * criticality_score playbook - prepare, publish report function is pending and in WIP
    
    * Added spdx-sbom-generator playbook  (#31)
    
    * Add files via upload
    
    * Added variables and other updations
    
    * Updated
    
    * Moved to playbooks dir
    
    * Updated code for cleanup
    
    * Added dummy data
    
    * Renamed to artifacts and added function to fetch steps file
    
    * formatted
    
    * Updated
    
    * Removed dummy data
    
    * Changed var
    
    * Changed var
    
    * Updated
    
    * Updated
    
    * Updated vars
    
    * Updated playbook names
    
    * Updated var names
    
    * Changed file name
    
    * Fixed errors
    
    * Cleaned up
    
    * Removed dummy data
    
    * criticality_score playbook - with new structure
    
    * criticality_score playbook
    
    * criticality_score playbook - removed old script
    
    * updated beslab owner type and name in env
    
    * commented tool path check as it is diff for criticality score
    
    * criticality_score playbook
    
    * no conflict
    
    * updated version as per feedback in PR
    
    * clean up code  - making it uncommented
    
    * addressed PR comment for checking tool installation
    
    * syntax error resolved
    
    ---------
    
    Co-authored-by: Arun Suresh <58501088+asa1997@users.noreply.github.com>

---
+++ playbooks/besman-criticality_score-0.0.1-steps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Running $ASSESSMENT_TOOL_NAME"
+cd "$BESMAN_TOOL_PATH" || return 1
+criticality_score -depsdev-disable -format json $BESMAN_ARTIFACT_URL | grep -o '{"default_score":.*}' > "$DETAILED_REPORT_PATH" 2>&1
+if [[ "$?" != "0" ]]; then
+    export CRITICALITY_SCORE_RESULT=1
+else
+    export CRITICALITY_SCORE_RESULT=0
+fi
