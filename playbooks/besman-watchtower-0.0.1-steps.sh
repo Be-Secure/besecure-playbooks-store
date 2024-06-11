699bc58891a018a0fe42c579d52dd0ee7496bfa8
Pramit Kumar Dutta <pramit.dutta@wipro.com>
2024-06-11 15:36:12 +0530

    updated config variable names (#63)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-15-80.ap-south-1.compute.internal>

--- playbooks/besman-watchtower-0.0.1-steps.sh
+++ playbooks/besman-watchtower-0.0.1-steps.sh
@@ -3,12 +3,12 @@
 echo "Running $ASSESSMENT_TOOL_NAME"
 source "$HOME/watchtower_env/bin/activate"
 
-if [[ "$BESMAN_REPO_TYPE" == "github" ]]; then
-    SCAN_CMD="python $BESMAN_WATCHTOWER_PATH/src/watchtower.py --repo_type=github --repo_url=$BESMAN_REPO_URL --branch_name=$BESMAN_BRANCH_NAME --depth=$BESMAN_DEPTH_VAL"
-elif [[ "$BESMAN_REPO_TYPE" == "huggingface" ]]; then
-    SCAN_CMD="python $BESMAN_WATCHTOWER_PATH/src/watchtower.py --repo_type=huggingface --repo_url=$BESMAN_REPO_URL"
+if [[ "$BESMAN_TARGET_REPO_TYPE" == "github" ]]; then
+    SCAN_CMD="python $BESMAN_WATCHTOWER_PATH/src/watchtower.py --repo_type=github --repo_url=$BESMAN_TARGET_REPO_URL --branch_name=$BESMAN_TARGET_BRANCH_NAME --depth=$BESMAN_TARGET_DEPTH_VAL"
+elif [[ "$BESMAN_TARGET_REPO_TYPE" == "huggingface" ]]; then
+    SCAN_CMD="python $BESMAN_WATCHTOWER_PATH/src/watchtower.py --repo_type=huggingface --repo_url=$BESMAN_TARGET_REPO_URL"
 else
-    __besman_echo_red "Unknown repository type: $BESMAN_REPO_TYPE"
+    __besman_echo_red "Unknown repository type: $BESMAN_TARGET_REPO_TYPE"
     export SCAN_RESULT=1
     return 1
 fi
