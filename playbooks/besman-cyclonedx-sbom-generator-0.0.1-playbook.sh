240cda286c64ee06f36c6a1fcbf2e92a836f80c3
NeerajK007 <neeraj.kumar32@wipro.com>
2025-01-17 15:24:26 +0530

    updated version

--- playbooks/besman-cyclonedx-sbom-generator-0.0.1-playbook.sh
+++ playbooks/besman-cyclonedx-sbom-generator-0.0.1-playbook.sh
@@ -4,7 +4,7 @@ function __besman_init() {
     __besman_echo_white "initialising"
     export ASSESSMENT_TOOL_NAME="cyclonedx-sbom-generator"
     export ASSESSMENT_TOOL_TYPE="sbom"
-    export ASSESSMENT_TOOL_VERSION="v11.0.10"
+    export ASSESSMENT_TOOL_VERSION="0.0.1"
     export ASSESSMENT_TOOL_PLAYBOOK="besman-$ASSESSMENT_TOOL_NAME-0.0.1-playbook.sh"
     
     local steps_file_name="besman-$ASSESSMENT_TOOL_NAME-0.0.1-steps.sh"
