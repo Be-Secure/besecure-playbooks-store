9f116a7b85233b75a93b4dc44f1af070b12d2ff3
NeerajK007 <neeraj.kumar32@wipro.com>
2025-04-09 17:21:14 +0530

    venv check before playbook execution to avoid missing dep error

--- playbooks/besman-llmSecurityBenchmark-frr-0.0.1-steps.sh
+++ playbooks/besman-llmSecurityBenchmark-frr-0.0.1-steps.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-echo "Running $ASSESSMENT_TOOL_NAME-$ASSESSMENT_TOOL_TYPE"
+__besman_echo_white "Running $ASSESSMENT_TOOL_NAME-$ASSESSMENT_TOOL_TYPE"
+
 cd "$BESMAN_TOOL_PATH" || return 1
+
+## Activate venv
+if [[ ! -d ~/.venvs/CybersecurityBenchmarks ]]; then
+    __besman_echo_red "[ERROR] Python virtual environment for CybersecurityBenchmarks is missing."
+    (return 1 2>/dev/null) || exit 1
+fi
+
+source ~/.venvs/CybersecurityBenchmarks/bin/activate
+
 python3 -m CybersecurityBenchmarks.benchmark.run \
     --benchmark=frr \
     --prompt-path="$BESMAN_CYBERSECEVAL_DATASETS/frr/frr.json" \
@@ -9,8 +19,9 @@ python3 -m CybersecurityBenchmarks.benchmark.run \
     --stat-path="$BESMAN_RESULTS_PATH/frr_stat.json" \
     --llm-under-test="$BESMAN_ARTIFACT_PROVIDER::$BESMAN_ARTIFACT_NAME:$BESMAN_ARTIFACT_VERSION::dummy_value" \
     --run-llm-in-parallel \
-    --num-test-cases=$BESMAN_NUM_TEST_CASES_FRR
-if [[ "$?" != "0" ]]; then
+    --num-test-cases="$BESMAN_NUM_TEST_CASES_FRR"
+
+if [[ "$?" -ne 0 ]]; then
     export FRR_RESULT=1
 else
     export FRR_RESULT=0
