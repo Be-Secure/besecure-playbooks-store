869b449970653705f7d1809ad5155ffd10a5fc30
asa1997 <arunsureshampadath@gmail.com>
2025-05-06 12:58:00 +0530

    changed condition

--- playbooks/besman-LLMSecAutocomplete-cyberseceval-steps-0.0.1.sh
+++ playbooks/besman-LLMSecAutocomplete-cyberseceval-steps-0.0.1.sh
@@ -28,7 +28,7 @@ else
     jq 'to_entries[0].value' "$BESMAN_RESULTS_PATH/autocomplete_stat.json" >"$BESMAN_RESULTS_PATH/autocomplete_stat.tmp.json" && mv "$BESMAN_RESULTS_PATH/autocomplete_stat.tmp.json" "$BESMAN_RESULTS_PATH/autocomplete_stat.json"
 fi
 
-if [[ "$AUTOCOMPLETE_RESULT" == "0" ]]; then
+if [[ -f "$BESMAN_RESULTS_PATH/autocomplete_responses.json" ]]; then
     # Copy result to detailed report path
     mv "$BESMAN_RESULTS_PATH/autocomplete_stat.json" "$AUTOCOMPLETE_TEST_REPORT_PATH/$BESMAN_ARTIFACT_NAME:$BESMAN_ARTIFACT_VERSION-autocomplete-test-summary-report.json.json"
     mv "$BESMAN_RESULTS_PATH/autocomplete_responses.json" "$AUTOCOMPLETE_TEST_REPORT_PATH/$BESMAN_ARTIFACT_NAME:$BESMAN_ARTIFACT_VERSION-autocomplete-test-detailed-report.json.json"
