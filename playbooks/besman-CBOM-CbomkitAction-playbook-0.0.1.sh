4d54d027378cef71ff5c132524b6499ebdd09fb9
Neeraj Kumar <neeraj.kumar32@wipro.com>
2025-06-03 11:09:01 +0530

    cryptopraphic playbook using cbomkit action (#117)
    * cryptopraphic playbook using cbomkit action
    
    * updated playbook - cbom - removed fetch step file function

---
+++ playbooks/besman-CBOM-CbomkitAction-playbook-0.0.1.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+function __besman_init() {
+    __besman_echo_white "initialising"
+    export ASSESSMENT_TOOL_NAME="CbomkitAction"
+    export ASSESSMENT_TOOL_TYPE="CBOM"
+    export ASSESSMENT_TOOL_VERSION="0.0.1"
+    export ASSESSMENT_TOOL_PLAYBOOK="besman-CBOM-CbomkitAction-playbook-0.0.1.sh"
+
+    local steps_file_name="besman-CBOM-CbomkitAction-steps-0.0.1.sh"
+    export BESMAN_STEPS_FILE_PATH="$BESMAN_PLAYBOOK_DIR/$steps_file_name"
+
+    local var_array=("BESMAN_ARTIFACT_TYPE" "BESMAN_ARTIFACT_NAME" "BESMAN_ARTIFACT_VERSION" "BESMAN_ARTIFACT_URL" "BESMAN_ENV_NAME" "BESMAN_ARTIFACT_DIR" "ASSESSMENT_TOOL_NAME" "ASSESSMENT_TOOL_TYPE" "ASSESSMENT_TOOL_VERSION" "ASSESSMENT_TOOL_PLAYBOOK" "BESMAN_ASSESSMENT_DATASTORE_DIR" "BESMAN_ASSESSMENT_DATASTORE_URL" "BESMAN_LAB_TYPE" "BESMAN_LAB_NAME")
+
+    local flag=false
+    for var in "${var_array[@]}"; do
+        if [[ ! -v $var ]]; then
+
+            # read -rp "Enter value for $var:" value #remove
+            # export "$var"="$value" #remove
+            __besman_echo_yellow "$var is not set" #uncomment
+            __besman_echo_no_colour ""             #uncomment
+            flag=true                              #uncomment
+        fi
+
+    done
+
+    local dir_array=("BESMAN_ASSESSMENT_DATASTORE_DIR")
+
+    for dir in "${dir_array[@]}"; do
+        # Get the value of the variable with the name stored in $dir
+        dir_path="${!dir}"
+
+        if [[ ! -d $dir_path ]]; then
+
+            __besman_echo_red "Could not find $dir_path"
+
+            flag=true
+
+        fi
+
+    done
+
+    if [[ -z $BESMAN_GH_TOKEN ]]; then
+        __besman_echo_red "GitHub Auth token not found"
+        __besman_echo_white ""
+        __besman_echo_white "Run the below command to set it"
+        __besman_echo_white ""
+        __besman_echo_yellow "$ export BESMAN_GH_TOKEN=<token>"
+        __besman_echo_white ""
+        flag=true
+    fi
+
+    # [[ ! -f $BESMAN_TOOL_PATH/$ASSESSMENT_TOOL_NAME ]] && __besman_echo_red "Could not find artifact @ $BESMAN_TOOL_PATH/$ASSESSMENT_TOOL_NAME" && flag=true
+
+    if [[ $flag == true ]]; then
+
+        return 1
+
+    else
+        export CBOM_PATH="$BESMAN_ASSESSMENT_DATASTORE_DIR/$BESMAN_ARTIFACT_NAME/$BESMAN_ARTIFACT_VERSION/cryptography"
+        export DETAILED_REPORT_PATH="$CBOM_PATH/$BESMAN_ARTIFACT_NAME-$BESMAN_ARTIFACT_VERSION-cryptography-report.json"
+        mkdir -p "$CBOM_PATH"
+        export OSAR_PATH="$BESMAN_ASSESSMENT_DATASTORE_DIR/$BESMAN_ARTIFACT_NAME/$BESMAN_ARTIFACT_VERSION/$BESMAN_ARTIFACT_NAME-$BESMAN_ARTIFACT_VERSION-osar.json"
+
+        return 0
+
+    fi
+
+}
+
+function __besman_execute() {
+    local duration
+    __besman_echo_yellow "Launching steps file"
+
+    SECONDS=0
+    . "$BESMAN_STEPS_FILE_PATH"
+    duration=$SECONDS
+    export EXECUTION_DURATION=$duration
+
+}
+
+function __besman_prepare() {
+
+    EXECUTION_TIMESTAMP=$(date)
+    export EXECUTION_TIMESTAMP
+    __besman_echo_white "preparing data"
+
+    __besman_generate_osar
+
+}
+
+function __besman_publish() {
+    __besman_echo_yellow "Pushing to datastores"
+    # push code to remote datastore
+    cd "$BESMAN_ASSESSMENT_DATASTORE_DIR"
+
+    git add "$DETAILED_REPORT_PATH" "$OSAR_PATH"
+    git commit -m "Added osar and detailed report for CBOM"
+    git push origin main
+    # Fix code
+    # gh pr create --title "Added reports" --body "Added osar and detailed reports"
+
+}
+
+function __besman_cleanup() {
+    local var_array=("ASSESSMENT_TOOL_NAME" "ASSESSMENT_TOOL_TYPE" "ASSESSMENT_TOOL_PLAYBOOK" "ASSESSMENT_TOOL_VERSION" "OSAR_PATH" "CBOM_PATH" "DETAILED_REPORT_PATH")
+
+    for var in "${var_array[@]}"; do
+        if [[ -v $var ]]; then
+            unset "$var"
+        fi
+
+    done
+}
+
+function __besman_launch() {
+    __besman_echo_yellow "Starting playbook"
+    local flag=1
+
+    __besman_init
+    flag=$?
+    if [[ $flag == 0 ]]; then
+
+        __besman_execute
+        flag=$?
+
+    else
+
+        __besman_cleanup
+        return
+    fi
+
+    if [[ $flag == 0 ]]; then
+
+        __besman_prepare
+        __besman_publish
+        __besman_cleanup
+
+    else
+
+        __besman_cleanup
+        return
+
+    fi
+}
