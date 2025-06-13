03807bdc54f0198bc2a23d6c9e9f492fa38131e4
sudhirverma <sudhirvvermaa@gmail.com>
2025-06-13 06:40:49 +0000

    fix setp file

--- playbooks/besman-LLMSecAutocomplete-cyberseceval-steps-0.0.1.sh
+++ playbooks/besman-LLMSecAutocomplete-cyberseceval-steps-0.0.1.sh
@@ -68,7 +68,8 @@ function __besman_run_assessment_in_background() {
         export AUTOCOMPLETE_RESULT=0 # You can later check status using PID
         return 0
     else
-        nohup "${python_command[@]}" 2>&1 | tee "$log_file" &
+        "${python_command[@]}"
+        echo "ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
         exit_code=$?
 
         if [[ "$exit_code" -ne 0 ]]; then
